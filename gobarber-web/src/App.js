fecadf642d63818599d2385e4973c8fa3a2165b0
Gabriel Masson <g.masson010@gmail.com>
2019-11-29 18:54:37 -0300

    Aplicação front-end em ReactJS

---
+++ gobarber-web/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Routes from './routes';
+import history from './services/history';
+
+import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
+}
