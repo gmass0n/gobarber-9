4212e4eac6a4f20aa17894e1f6e46039e2472e9d
Gabriel Masson <g.masson010@gmail.com>
2019-11-29 18:54:00 -0300

    Aplicação front-end em React Native

---
+++ gobarber-mobile/src/routes.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+const NewStack = createStackNavigator(
+  {
+    SelectProvider,
+    SelectDateTime,
+    Confirm,
+  },
+  {
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerTransparent: true,
+      headerTintColor: '#fff',
+      headerLeftContainerStyle: {
+        marginLeft: 20,
+      },
+    },
+  }
+);
+
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            New: {
+              screen: NewStack,
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
+          {
+            // TODA VEZ QUE SAIR DE UMA ROTA ELA IRA RESETAR
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
